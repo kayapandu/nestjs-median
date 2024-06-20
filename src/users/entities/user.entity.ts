222ef39135b034c183c89f091790631d14326b7e
panduartyan <panduartyan@gmail.com>
2024-06-20 10:55:35 +0700

    feat(Project):  add initial commit for project

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { User } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
